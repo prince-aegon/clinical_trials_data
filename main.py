4a9e367d36ba7a67cb854aafcb642c8255405723
Sarthak Jha <jhasarthak123@gmail.com>
2023-08-29 22:37:16 +0530

    Added facility location data

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def get_enrollment(file_name):
         return (enrollment_type, enrollment_value)
 
     except Exception as e:
-        print(e)
+        # print(e)
         return -1
     
 def get_completion_status(file_name):
@@ -30,7 +30,7 @@ def get_completion_status(file_name):
         overall_status = overall_status_element.text
         return overall_status
     except Exception as e:
-        print(e)
+        # print(e)
         return -1
     
 def get_average(date1, date2):
@@ -70,6 +70,34 @@ def get_date_format(file_name):
             return (0, start_date_element.text, end_date_element.text)
     except Exception as e:
         return (-1, -1, -1)
+    
+
+def get_address_of_trial(file_name):
+    tree = ET.parse(f'{file_name}')
+    root = tree.getroot()
+
+    facility_element = root.find('location/facility')
+    response = {}
+
+    try:
+        name_of_facility = facility_element.find('name').text
+        response["name"] = name_of_facility
+    except Exception as e:
+        response["name"] = ""
+    
+    try:
+        address_of_facility = facility_element.find('address')
+        location_of_facility = {}
+        for params in address_of_facility:
+            location_of_facility[params.tag] = params.text
+        response["address"] = location_of_facility
+    except Exception as e:
+        response["address"] = {}
+
+
+    return response
+
+    
 
 current_dir = os.getcwd()
 folder_path = f'{current_dir}/data'
@@ -81,18 +109,24 @@ if os.path.exists(folder_path) and os.path.isdir(folder_path):
     enrollment_type_count = {}
     date_formats_for_duration = [0] * 3
     number_of_months = []
+    count_facility_name, count_facility_address = 0, 0
+
     for file_name in file_names:
+        if not file_name.endswith(".xml"):
+            continue
         total_files += 1
+        
         result = get_enrollment(f'{folder_path}/{file_name}')
         if result == -1:
-            print(f'{file_name} -> Enrollment tag absent')
+            pass
+            # print(f'{file_name} -> Enrollment tag absent')
         else:
             enrollment_tags_present += 1
             if result[0] in enrollment_type_count:
                 enrollment_type_count[result[0]] += 1
             else:
                 enrollment_type_count[result[0]] = 1
-            print(f'{file_name} -> {result[0]}, {result[1]} ')
+            # print(f'{file_name} -> {result[0]}, {result[1]}')
 
         
         duration = get_completion_status(f'{folder_path}/{file_name}')
@@ -107,6 +141,16 @@ if os.path.exists(folder_path) and os.path.isdir(folder_path):
             if format[0] == 2:
                 number_of_months.append(get_average(format[1], format[2]))
 
+        
+        # Get address
+        facilities = get_address_of_trial(f'{folder_path}/{file_name}')
+        if facilities["name"] != "":
+            count_facility_name += 1
+        if facilities["address"] != {}:
+            count_facility_address += 1
+        # print(f'{file_name} -> {facilities}')
+
+
             
     
 
@@ -120,7 +164,13 @@ print(f'Date formats : {date_formats_for_duration}')
 
 freq_of_months = np.bincount(number_of_months)
 
+plt.title('Frequency of Trial Duration')
+plt.ylabel('Count')
+plt.xlabel('Number of Months')
 plt.hist(freq_of_months, bins=50, edgecolor='black')
 # plt.bar(range(len(freq_of_months)), freq_of_months, tick_label=range(len(freq_of_months)))
 plt.show()
-print(min(number_of_months), max(number_of_months))
+# print(min(number_of_months), max(number_of_months))
+
+
+print(f'Number of facilities with name -> {count_facility_name}, address -> {count_facility_address}')
