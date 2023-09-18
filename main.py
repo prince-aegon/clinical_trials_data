b962182809981de95145efa53230610302ee88f5
aryansh085 <aryanshsingla@gmail.com>
2023-09-18 11:32:21 +0530

    Changes related to time and duration

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from datetime import datetime
+
+import string
+
+# # Your input text
+# text = "Hello, this is an example text with some punctuation marks! How are you?"
+
+# # Remove punctuation using string.punctuation
+# translator = str.maketrans(' ', '', string.punctuation)
+# cleaned_text = text.translate(translator)
+
+# # Print the cleaned text
+# print(cleaned_text)
+
 
 
 # global variables
@@ -28,6 +42,7 @@ enrollment_rate_list = []
 duration_list = []
 start_date_list = []
 completion_date_list = []
+text_data = []
 
 
 def get_enrollment(file_name):
@@ -83,37 +98,73 @@ def get_date_format(file_name):
     root = tree.getroot()
 
     start_date_element = root.find('start_date')
-    
     end_date_element = root.find('completion_date')
     end_date_completion_element = root.find('primary_completion_date')
-
+    date_formats = ["%Y-%m", "%m/%Y", "%m-%Y","%Y %m", "%m %Y", "%m %Y","%m %d, %Y"]
     
     pattern = r"^(January|February|March|April|May|June|July|August|September|October|November|December) \d{4}$"
-    try:
-        if root.find('completion_date') is None and root.find('primary_completion_date') is not None:
-            end_date_element = end_date_completion_element
-        
-        if re.match(pattern, start_date_element.text) and re.match(pattern, end_date_element.text):
-            start_date_list.append(start_date_element.text)
-            completion_date_list.append(end_date_element.text)
+    if root.find('completion_date') is None and root.find('primary_completion_date') is not None:
+        end_date_element = end_date_completion_element
 
-            return (2, start_date_element.text, end_date_element.text)
-        elif re.match(pattern, start_date_element.text) or re.match(pattern, end_date_element.text):
-            start_date_list.append(start_date_element.text)
-            completion_date_list.append(end_date_element.text)
-            
-            return (1, start_date_element.text, end_date_element.text)
-        else:
-            start_date_list.append(start_date_element.text)
-            completion_date_list.append(end_date_element.text)
+    # print(start_date_element.text)
+    # print(end_date_element.text)
+    flag=0
+    for date_format in date_formats:
+        try:
+            date_obj = datetime.strptime(start_date_element.text, date_format)
+            date_obj_end = datetime.strptime(end_date_element.text, date_format)
 
-            return (0, start_date_element.text, end_date_element.text)
-    except Exception as e:
+            start_date_list.append(date_obj)
+            completion_date_list.append(date_obj_end)
+            flag = 1
+            return 2
+        except ValueError:
+                pass
+    if (flag==0):
         start_date_list.append("00")
         completion_date_list.append("00")
 
-        return (-1, -1, -1)
     
+        # if re.match(pattern, start_date_element.text) and re.match(pattern, end_date_element.text):
+        #     start_date_list.append(start_date_element.text)
+        #     completion_date_list.append(end_date_element.text)
+
+        #     return (2, start_date_element.text, end_date_element.text)
+        # elif re.match(pattern, start_date_element.text) or re.match(pattern, end_date_element.text):
+        #     start_date_list.append(start_date_element.text)
+        #     completion_date_list.append(end_date_element.text)
+            
+        #     return (1, start_date_element.text, end_date_element.text)
+        # else:
+        #     start_date_list.append(start_date_element.text)
+        #     completion_date_list.append(end_date_element.text)
+
+        #     return (0, start_date_element.text, end_date_element.text)
+    # except Exception as e:
+
+        # start_date_list.append("00")
+        # completion_date_list.append("00")
+
+        # return (-1, -1, -1)
+    
+def get_criteria(file_name):
+    tree = ET.parse(f'{file_name}')
+    root = tree.getroot()
+    try:
+        eligibility_details = root.find('eligibility')
+        criteria = eligibility_details.find('criteria')
+        text_data = criteria.find('textblock').text
+        return text_data
+        # if gender in gender_details:
+        #     gender_details[gender] += 1
+        # else:
+        #     gender_details[gender] = 1
+        # print(f'{file_name} -> {gender} ')
+
+    except Exception as e:
+        print(e)
+        return ""
+
 
 def get_address_of_trial(file_name):
     tree = ET.parse(f'{file_name}')
@@ -164,7 +215,7 @@ def get_eligibility_details(file_name):
             gender_details[gender] += 1
         else:
             gender_details[gender] = 1
-        print(f'{file_name} -> {gender} ')
+        # print(f'{file_name} -> {gender} ')
 
     except Exception as e:
         print(e)
@@ -176,7 +227,7 @@ def get_eligibility_details(file_name):
             min_age_details[min_age] += 1
         else:
             min_age_details[min_age] = 1
-        print(f'{file_name} -> {min_age} ')
+        # print(f'{file_name} -> {min_age} ')
 
     except Exception as e:
         print(e)
@@ -188,7 +239,7 @@ def get_eligibility_details(file_name):
             max_age_details[max_age] += 1
         else:
             max_age_details[max_age] = 1
-        print(f'{file_name} -> {max_age} ')
+        # print(f'{file_name} -> {max_age} ')
 
     except Exception as e:
         print(e)
@@ -200,11 +251,11 @@ folder_path = f'{current_dir}/data'
 
 if os.path.exists(folder_path) and os.path.isdir(folder_path):
 
-    enrollment_count = 0
-    num_of_days = 1
     file_names = os.listdir(folder_path)
     for file_name in file_names:
-        
+        enrollment_count = 0
+        num_of_days = 1
+
         if not file_name.endswith(".xml"):
             continue
         total_files += 1
@@ -215,7 +266,6 @@ if os.path.exists(folder_path) and os.path.isdir(folder_path):
 
         result = get_enrollment(f'{folder_path}/{file_name}')
         if result == -1:
-
             pass
             # print(f'{file_name} -> Enrollment tag absent')
         else:
@@ -230,21 +280,27 @@ if os.path.exists(folder_path) and os.path.isdir(folder_path):
         
         duration = get_completion_status(f'{folder_path}/{file_name}')
 
-
         if duration == 'Completed':
             completed_status += 1
 
         format = get_date_format(f'{folder_path}/{file_name}')
-        if format[0] != -1:
-            date_formats_for_duration[format[0]] += 1
-            if format[0] == 2:
-                month_freq = get_average(format[1], format[2])
-                number_of_months.append(month_freq)
-                num_of_days = 30*month_freq
-                num_of_days = max(num_of_days,1)
+        # if format[0] != -1:
+        #     date_formats_for_duration[format[0]] += 1
+        #     if format[0] == 2:
+        #         month_freq = get_average(format[1], format[2])
+        #         number_of_months.append(month_freq)
+        #         num_of_days = 30*month_freq
+        #         num_of_days = max(num_of_days,1)
         
         enrollment_rate_list.append(int(enrollment_count)/num_of_days)
         duration_list.append(num_of_days)
+
+        # Get Textual Data
+        text_dt = get_criteria(f'{folder_path}/{file_name}')
+        if text_dt !="":
+            text_data.append(text_dt)
+        else:
+            print()
 
         # Get address
         facilities = get_address_of_trial(f'{folder_path}/{file_name}')
@@ -259,43 +315,43 @@ if os.path.exists(folder_path) and os.path.isdir(folder_path):
                 countries_of_facilities[location_country["country"]] += 1
             else:
                 countries_of_facilities[location_country["country"]] = 1
-        print(f'{file_name} -> {location_country}')
+        # print(f'{file_name} -> {location_country}')
 
-print(len(file_name_list))
-print(len(enrollment_rate_list))
-print(len(start_date_list))
-print(len(completion_date_list))
+# print(len(file_name_list))
+# print(len(enrollment_rate_list))
+# print(len(start_date_list))
+# print(len(completion_date_list))
 dict = {'file_name': file_name_list, 'enrollment_rate': enrollment_rate_list, 'duration': duration_list, 'start_date': start_date_list, 'completion_date':completion_date_list}
 
 df = pd.DataFrame(dict)
 df.to_csv('file1.csv')
           
 
-print(f'Total files : {total_files}')
-print(f'Number of Actual, Anticipated, None tags: {enrollment_type_count}')
-print(f'Number of files with enrollment tag : {enrollment_tags_present}')
+# print(f'Total files : {total_files}')
+# print(f'Number of Actual, Anticipated, None tags: {enrollment_type_count}')
+# print(f'Number of files with enrollment tag : {enrollment_tags_present}')
 
-print(f'Completed studies : {completed_status}')
+# print(f'Completed studies : {completed_status}')
 
-print(f'Date formats : {date_formats_for_duration}')
+# print(f'Date formats : {date_formats_for_duration}')
 
-freq_of_months = np.bincount(number_of_months)
+# freq_of_months = np.bincount(number_of_months)
 
-plt.title('Frequency of Trial Duration')
-plt.ylabel('Count')
-plt.xlabel('Number of Months')
-plt.hist(freq_of_months, bins=50, edgecolor='black')
-# plt.bar(range(len(freq_of_months)), freq_of_months, tick_label=range(len(freq_of_months)))
-plt.show()
-# print(min(number_of_months), max(number_of_months))
+# plt.title('Frequency of Trial Duration')
+# plt.ylabel('Count')
+# plt.xlabel('Number of Months')
+# plt.hist(freq_of_months, bins=50, edgecolor='black')
+# # plt.bar(range(len(freq_of_months)), freq_of_months, tick_label=range(len(freq_of_months)))
+# plt.show()
+# # print(min(number_of_months), max(number_of_months))
 
 
-print(f'Number of facilities with name -> {count_facility_name}, address -> {count_facility_address}')
-print(countries_of_facilities)
+# print(f'Number of facilities with name -> {count_facility_name}, address -> {count_facility_address}')
+# print(countries_of_facilities)
 
 
 for key in gender_details:
-    print(key, gender_details[key])
+    # print(key, gender_details[key])
     gender_count +=gender_details[key]
 
 for key in min_age_details:
@@ -309,8 +365,8 @@ for key in max_age_details:
 min_age_NA = min_age_details["N/A"]
 max_age_NA = max_age_details["N/A"]
 
-print(f'Total gender count is : {gender_count}')
-print(f'Total min_age_count is : {max_age_count}')
-print(f'Total max_age_count is : {min_age_count}')
-print(f'Total min_age_N/A count is: {min_age_NA}')
-print(f'Total max_age_N/A count is: {max_age_NA}')
+# print(f'Total gender count is : {gender_count}')
+# print(f'Total min_age_count is : {max_age_count}')
+# print(f'Total max_age_count is : {min_age_count}')
+# print(f'Total min_age_N/A count is: {min_age_NA}')
+# print(f'Total max_age_N/A count is: {max_age_NA}')
